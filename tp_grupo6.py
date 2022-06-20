b138b2a3cbfd1fae7f4edb6506f1a174d9f5675b
MissingNno <fdesimone@uade.edu.ar>
2022-06-19 21:31:29 -0300

    se agrego la funcion de relevar casillas y de validacion de datos

--- tp_grupo6.py
+++ tp_grupo6.py
@@ -1,4 +1,5 @@
 import random
+import time
 import os
 
 def generate_board(board_size: int, total_mines: int) -> tuple:
@@ -59,14 +60,93 @@ def show_board(board: list, letters: list) -> None:
         print(row + '\n' + line_h)
     print('')
 
+def reveal_cells(full_board,current_board,row,col):
+    if current_board[row][col] != ' ': return
+    current_board[row][col] = full_board[row][col]
+    if full_board[row][col]=='0':
+        for f,c in obtain_neighbors(full_board,row,col):
+            if current_board[f][c] != 'F':
+                reveal_cells(full_board,current_board,f,c)
+
+def validate_input(data,board_size):
+    cell = ()
+    flag = False
+    if len(data) >= 1 and len(data) <= 3:
+        if data[0].upper() in letters[:board_size]:
+            if data[1].isnumeric() and 0 < int(data[1]) <= board_size:
+                cell = (int(letters.index(data[0].upper())),int(data[1])-1)
+                if len(data) == 3:
+                    if data[2].upper() == 'F':
+                        flag = True
+    
+    return {"cell": cell,"flag":flag}
+
+
 board_size = 10
 total_mines = 20
+get_board, get_mines = generate_board(board_size,total_mines)
+flags = []
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-get_board, get_mines = generate_board(board_size,total_mines)
 last_board = [[' ' for i in range(board_size)] for i in range(board_size)]
-board = []
-flags = []
 
-show_board(get_board,letters)
+print()
+print("Buscaminas - Juego Realizado por Grupo 11".center(100,"*"),end="\n\n")
+show_board(last_board,letters)
+message = "Instrucciones:\n" + "> Escriba la columna y luego la fila (ej. c4).\n" + "> Para colocar o quitar una bandera, agrege 'f' a la celda (ej. a5f)."
+print(message)
+
+while True:
+        mines_left = total_mines - len(flags)
+        data = input('Ingresar Celda ({} minas restantes): '.format(mines_left))
+        result = validate_input(data, board_size)
+
+        cell = result['cell']
+        print(cell)
+
+        if cell:
+            print('\n\n')
+            col, row = cell
+            selected_cell = last_board[row][col]
+            flag = result['flag']
+
+            if flag:
+                if selected_cell == ' ':
+                    last_board[row][col] = 'F'
+                    flags.append(cell)
+                elif selected_cell == 'F':
+                    last_board[row][col] = ' '
+                    flags.remove(cell)
+                else:
+                    message = 'No se puede colocar una bandera aqui'
+
+            # If there is a flag there, show a message
+            elif cell in flags:
+                message = 'Ya existe una bandera en esa casilla'
+
+            elif get_board[row][col] == 'X':
+                print('Has Perdido!\n')
+                show_board(get_board,letters)
+                #if playagain():
+                    #playgame()
+                #return
+
+            elif selected_cell == ' ':
+                reveal_cells(get_board, last_board, row, col)
+
+            else:
+                message = "Esta celda ya ha sido revelada!"
+
+            if set(flags) == set(get_mines):
+                print(
+                    'Ganaste!'
+                    #'It took you {} minutes and {} seconds.\n'.format(minutes,seconds)
+                    )
+                show_board(get_board,letters)
+                #if playagain():
+                    #playgame()
+                #return
+
+        show_board(last_board,letters)
+        print(message)
